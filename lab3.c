b9069471243315889c1eeb8fd20023173e67d546
Kakishbolshoy <145188328+Kakishbolshoy@users.noreply.github.com>
2023-12-16 12:49:52 +0500

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -15,30 +15,35 @@ int main()
     for (i = 0; i < n; i++) {
         a[i] = rand() % 101 - 50;
     }
-
+    printf("Массив: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    
     int mult;
     int sum;
     for (i = 0; i < n; i++) {
         mult = 1;
         sum = 0;
         int k;
-        for (k = 2; k < n; k++) {
-            if (a[i]==a[k]) {
+        for (k = i + 1; k < n; k++) {
+            if (a[i] == a[k]) {
                 sum += a[k - 1];
                 mult *= a[k - 1];
             }
         }
+        printf("Между элементами %d и %d: ", i, k);
         printf("Сумма равна: %d\n", sum);
-        if (mult==1){
-            printf("произведение равно 0 \n");
+        if (mult == 1) {
+            printf("Произведение равно 0\n");
+        } else {
+            printf("Произведение равно: %d\n", mult);
         }
-        else{
-        printf("Произведение равно: %d\n", mult);
         printf("\n");
-        }
     }
 
     return 0;
 }
-
+    
     
