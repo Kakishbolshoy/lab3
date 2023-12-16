bec5406690ca18ae3c6b0f0b817d0309479f5bd9
Kakishbolshoy <145188328+Kakishbolshoy@users.noreply.github.com>
2023-12-16 14:18:41 +0500

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -4,52 +4,59 @@
 
 int main()
 {
-    srand(time(NULL));
+srand(time(NULL));
 
-    int n;
-    printf("Введите количество элементов: ");
-    scanf("%d", &n);
+int n;
+printf("Введите количество элементов: ");
+scanf("%d", &n);
 
-    int a[n];
-    int i;
-    for (i = 0; i < n; i++) {
-        a[i] = rand() % 101 - 50;
+int a[n];
+int i;
+for (i = 0; i < n; i++) {
+    a[i] = rand() % 101 - 50;
     }
-    printf("Массив: ");
-    for (i = 0; i < n; i++) {
-        printf("%d ", a[i]);
+printf("начальный массив: ");
+for (i = 0; i < n; i++) {
+    printf("%d ", a[i]);
     }
-    printf("\n");
-    
-    int mult;
-    int sum;
-    for (i = 0; i < n; i++) {
-        mult = 1;
-        sum = 0;
-        int k;
-        for (k = i + 1; k < n; k++) {
-            if (a[i] == a[k]) {
-                sum += a[k - 1];
-                mult *= a[k - 1];
+printf("\n");
+
+int start = -1;
+int end = -1;
+int mult = 1;
+int sum = 0;
+for (i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+        if (a[i] == a[j]) {
+            start = i;
+            end = j;
+            break;
             }
-        }
-        if (sum!=0) {
-            printf("Между элементами %d и %d: ", i, k);
-            printf("Сумма равна: %d\n", sum);
-            if (mult == 1) {
-                printf("Произведение равно 0\n");
-            } else {
-                printf("Произведение равно: %d\n", mult);
-            }
-            printf("\n"); 
-        }
-        a[k]=mult;
-        a[i]=sum;
-        for (i = 0; i < n; i++) {
-        printf("%d ", a[i]);
-        }
-    }
-
-    return 0;
 }
- 
+if (start != -1 && end != -1) {
+    for (int k = start + 1; k < end; k++) {
+        sum += a[k];
+        mult *= a[k];
+        }
+break;
+}
+}
+
+printf("Между индексами %d и %d: ", start, end);
+printf("Сумма равна: %d\n", sum);
+printf("Произведение равно: %d\n", mult);
+printf("\n");
+
+if (start != -1 && end != -1) {
+    a[start] = sum;
+    a[end] = mult;
+    }
+
+printf("конечный массив: ");
+for (i = 0; i < n; i++) {
+    printf("%d ", a[i]);
+    }
+printf("\n");
+
+return 0;
+}
